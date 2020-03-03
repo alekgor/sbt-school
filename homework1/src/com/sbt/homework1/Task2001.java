0f326b48e6d3b0836fbbbd712be60b4351cd0844
alekgor <alexsgorbenko@yandex.ru>
2020-03-03 17:13:24 +0300

    refactor

--- homework1/src/com/sbt/homework1/Task2001.java
+++ homework1/src/com/sbt/homework1/Task2001.java
@@ -1,16 +1,18 @@
 package com.sbt.homework1;
 
-    import java.util.*;
-    import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    public class Task2001 {
-        public static void main(String[] argv) throws IOException {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(in.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = a + b;
+public class Task2001 {
+    public static void main(String[] argv) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = a + b;
 
-            System.out.println(c);
-        }
+        System.out.println(c);
     }
+}
