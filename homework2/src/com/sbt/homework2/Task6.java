f0b7ecd7e57185172f0cf993c1efc6a1357f20b3
alekgor <alexsgorbenko@yandex.ru>
2020-03-08 00:53:02 +0300

    add Task6.java

---
+++ homework2/src/com/sbt/homework2/Task6.java
@@ -0,0 +1,26 @@
+package com.sbt.homework2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Task6 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList<>(5000);
+        try (Scanner in = new Scanner(new FileInputStream("src/com/sbt/homework2/ww.txt"), "windows-1251")) {
+            while (in.hasNextLine()) {
+                list.add(in.nextLine());
+            }
+        }
+        Scanner keyboard = new Scanner(System.in);
+        while (keyboard.hasNextLine()) {
+            StringTokenizer st = new StringTokenizer(keyboard.nextLine());
+            while (st.countTokens() != 0) {
+                System.out.println(list.get(Integer.parseInt(st.nextToken())));
+            }
+        }
+    }
+}
