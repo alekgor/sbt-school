a9160442e9590d03dd8415dbb865c3225c32e115
alekgor <alexsgorbenko@yandex.ru>
2020-03-16 21:27:13 +0300

    Изменен: Exception project

--- Exception/src/com/sbt/exception/Main.java
+++ Exception/src/com/sbt/exception/Main.java
@@ -1,8 +1,56 @@
 package com.sbt.exception;
 
+import java.io.File;
+import java.util.Scanner;
+
+class MyException extends Exception {
+    public MyException() {
+        super("Считана пустая строка");
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+}
+
+
 public class Main {
 
+    public static String read() throws MyException {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        switch (line) {
+            case (""):
+                throw new MyException();
+            case (" "):
+                throw new MyException("Строка состоит из пробела");
+            default:
+                break;
+        }
+        return line;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        String line;
+        try {
+            System.out.println("Введите строку:");
+            line = Main.read();
+        } catch (MyException e) {
+            System.out.println("e.getMessage(): " + e.getMessage());
+            line = "0";
+        }
+
+        System.out.println(line + '\n');
+//        try-with-resources
+
+        System.out.println("Содержимое файла : ");
+        try (Scanner inFile = new Scanner(new File("src/com/sbt/exception/text.txt"))) {
+            while (inFile.hasNextLine()) {
+                System.out.println(inFile.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Не удалось открыть файл : " + e.getMessage());
+        }
     }
 }
